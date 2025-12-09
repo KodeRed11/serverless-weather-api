c484a7f9a74f9a4b6fbda062ceb1ba2a5c8ac871
eross11 <eross11@asu.edu>
2025-12-09 14:18:21 -0800

    Adding code for lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,27 @@
+import json
+import os
+import urllib.request
+
+def lambda_handler(event, context):
+    city = event.get('city', 'London')
+
+    api_key = os.getenv('OPENWEATHER_API_KEY')
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = json.loads(response.read())
+            weather = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description']
+            }
+            return {
+                'statusCode': 200,
+                'body': json.dumps(weather)
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
