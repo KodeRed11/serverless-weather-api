85abedf39429a537d3a5a061ecc0498a8432fda4
eross11 <eross11@asu.edu>
2025-12-09 15:13:04 -0800

    Updating lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,25 +3,24 @@ import os
 import urllib.request
 
 def lambda_handler(event, context):
-    city = event.get('city', 'London')
+    city = event.get("queryStringParameters", {}).get("city", "Phoenix")
 
-    api_key = os.getenv('OPENWEATHER_KEY')
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    api_key = os.environ["OPENWEATHER_KEY"]
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial"
 
     try:
         with urllib.request.urlopen(url) as response:
             data = json.loads(response.read())
-            weather = {
-                'city': data['name'],
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description']
-            }
             return {
-                'statusCode': 200,
-                'body': json.dumps(weather)
+                "statusCode": 200,
+                "body": json.dumps({
+                    "city": city,
+                    "temperature": data["main"]["temp"],
+                    "weather": data["weather"][0]["description"]
+                })
             }
     except Exception as e:
         return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
         }
